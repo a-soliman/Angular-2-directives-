7e5aac64242015a644d8f570cc675bc06d7bb3d6
Ahmed Soliman <ahmedanwar@mail.com>
2016-10-15 20:30:37 -0700

    practicing ngClass Attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'dir-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: [`
+	.border { 
+		border: 1px solid blue;
+	}
+	.background {
+		background-color: green;
+	}
+	div {
+		width: 100px;
+		height: 100px;
+	}
+
+  	`]
 })
 export class AppComponent {
-  title = 'dir works!';
+
 }
